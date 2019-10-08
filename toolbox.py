2ea93bb9f716f58f1f341ca6f32218c2dcbc1b5e
Jules Huisman <huisman.jules@gmail.com>
2019-10-08 11:27:15 +0200

    Added caching functionality of database data

--- toolbox.py
+++ toolbox.py
@@ -1,9 +1,16 @@
 import pandas as pd
 import peewee as pw
+import os
+import re
 
 class ToolBox:
-    def __init__(self):
+    def __init__(self, cache_folder='./.db_cache/'):
         self.connect_db()
+        self.cache_folder = cache_folder
+
+        # Check if the cache folder exists
+        if not os.path.exists(self.cache_folder):
+            os.mkdir(self.cache_folder)
 
     def connect_db(self):
         """
@@ -19,6 +26,14 @@ class ToolBox:
 
         self.db.connect()
 
+    def create_cache_file_name(self, query):
+        return re.sub(r'[,!@#$=\ \']', '', query)
+
+    def cache_file(self, df, file_name):
+        cache_file = self.cache_folder + file_name + '.pkl'
+
+        df.to_pickle(cache_file)
+
     def load_data_raw_sql(self, query):
         """
         Transform a string into a sql query
@@ -29,13 +44,33 @@ class ToolBox:
 
         return pd.DataFrame(query)
 
-    def load_data_sql(self, select='*', table='', where='', limit=None):
+    def load_data_sql(self, select='*', table='', where='', limit=None, use_cache=True):
         """
         Function to select data from the database with certain parameters
         """
 
         limit = 'LIMIT {}'.format(limit) if limit else ''
 
-        query = 'SELECT {} FROM {} WHERE {} {}'.format(select, table, where, limit)
+        where = 'WHERE {}'.format(where) if where else ''
 
-        return self.load_data_raw_sql(query)
+        query = 'SELECT {} FROM {} {} {}'.format(select, table, where, limit)
+
+        # Cached file name
+        file_name = self.create_cache_file_name(query)
+        cache_file = self.cache_folder + file_name + '.pkl'
+
+        # Check if the cached file exists
+        if os.path.exists(cache_file) and use_cache:
+            print('Loaded from cache')
+            df = pd.read_pickle(cache_file)
+
+        # Otherwise load the data from the database
+        else:
+            print('Loading from database')
+            # Create the dataframe
+            df = self.load_data_raw_sql(query)
+
+            # Cache the file
+            self.cache_file(df, file_name)
+
+        return df
