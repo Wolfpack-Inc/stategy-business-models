6106dd5bd8321501fe6e0c1008a771dc78ada7b6
liesketrommelen <a.e.p.trommelen@student.tue.nl>
2019-11-20 15:57:52 +0100

    Update Hurdle models.R

--- regressions/Hurdle models.R
+++ regressions/Hurdle models.R
@@ -12,16 +12,6 @@ getwd()
 data <- read.csv("C:/Users/20190298/Documents/GitHub/strategy-business-models/final-df-count-models.csv")
 
 plot(table(data$nr_reviews))
-
-testje<-factor(data$genre)
-levels(testje)
-
-#factor should be lowercase
-data$genre <- tolower(data$genre)
-data$console <- tolower(data$console)
-
-test_data <- cbind(data,dummy_cols(data$genre),dummy_cols(data$console))
-colnames(test_data) <- c("game","nr_reviews","user_avg_grade","user_sentiment_var","critic_avg_grade","critic_sentiment_var","critic_volume","genre","console", ".data", "is_action","is_miscellaneous" ,"is_general","is_sports" , "is_role_playing" , "is_strategy" ,"is_action_adventure" ,"is_simulation", "is_adventure" ,"is_arcade" ,"is_racing" ,"is_driving" ,"is_puzzle", ".data2","is_pc","is_playstation_3","is_xbox_360","is_nintendo_64","is_switch","is_playstation_4","is_3ds","is_xbox_one","is_playstation_vita","is_wii_u","is_ds","is_playstation_2","is_wii","is_gamecube","is_ios","is_xbox","is_psp","is_game_boy_advance","is_playstation")
 # Hurdle model 1
 mod_hurdle <- hurdle(nr_reviews ~ user_sentiment_var * critic_sentiment_var + user_avg_grade + critic_avg_grade + critic_volume | user_sentiment_var * critic_sentiment_var + user_avg_grade + critic_avg_grade + critic_volume + factor(genre) + factor(console),
                      data = data)mod_hurdle <- hurdle(nr_reviews ~ user_sentiment_var * critic_sentiment_var + user_avg_grade + critic_avg_grade + critic_volume + is_action+is_miscellaneous +is_general+is_sports + is_role_playing + is_strategy + is_action_adventure + is_simulation + is_adventure + is_arcade + is_racing + is_driving, data = test_data)
